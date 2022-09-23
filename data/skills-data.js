45cfc5dad5b76ea84c30c6ef472d4410bda92e7c
EricSoklowski <ericsoko2002@gmail.com>
2022-09-23 00:27:26 -0400

    step 1 complete

---
+++ data/skills-data.js
@@ -0,0 +1,9 @@
+const skills = [
+  {text: 'unyielding swag', have: true, _id: 125223},
+  {text: 'being sick asf', have: true, _id: 127904},
+  {text: 'boring and dumb', have: false, _id: 139608},
+]
+
+export{
+  skills
+}
